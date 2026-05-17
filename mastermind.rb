a721d1cb92b836f8e6f63c9e6de1729cbf682e85
j-unisa <104635026+j-unisa@users.noreply.github.com>
2026-05-17 14:32:47 -0700

    docs: document mastermind game design
    - outline step for randomized 4-digit code generation
    - specify rule constraint of 10 maximum attempts
    - document requirements for position and value feedback
    - map out foundational loop for user guess evaluation

--- mastermind.rb
+++ mastermind.rb
@@ -1,1 +1,9 @@
 # Mastermind project
+
+# Randomly set order of 4 numbers between 1-6 (cpu is code maker)
+# Provide instructions to the user
+# User (code breaker) makes guess
+# Compare guess to code
+# Provide feedback
+# State which numbers and positions are correct
+# User continues to make guesses until code is completely guessed or 10 attempts have been made
