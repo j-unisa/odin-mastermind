719b7d549b037cd05303b2141432496e472c19eb
j-unisa <104635026+j-unisa@users.noreply.github.com>
2026-05-18 12:02:04 -0700

    feat: add code generation and user input capture
    - define array constants for valid code values
    - implement randomized 4-digit code generation
    - capture initial user guess as integer data type
    - add print statements for runtime debugging

--- mastermind.rb
+++ mastermind.rb
@@ -1,8 +1,21 @@
 # Mastermind project
 
 # Randomly set order of 4 numbers between 1-6 (cpu is code maker)
+CODE_NUMBERS = [1, 2, 3, 4, 5, 6]
+secret_code = CODE_NUMBERS.sample(4)
+
+p secret_code # Delete before going live
+
 # Provide instructions to the user
+puts "Welcome to Mastermind!"
+puts "Try to guess the 4 digit code from numbers 1 to 6."
+
 # User (code breaker) makes guess
+user_guess = gets.chomp.to_i
+
+puts "User's guess is #{user_guess}" # Delete before going live
+puts "User's guess is a(n) #{user_guess.class}" # Delete before going live
+
 # Compare guess to code
 # Provide feedback
 # State which numbers and positions are correct
