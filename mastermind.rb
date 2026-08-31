4a527fea5f7906170d02c552cb3646e07171697d
j-unisa <104635026+j-unisa@users.noreply.github.com>
2026-08-31 11:41:38 -0700

    feat: freeze code constant and add feedback check
    - freeze CODE_NUMBERS array constant
    - implement include? check to compare input with code
    - output basic success/failure messages based on match

--- mastermind.rb
+++ mastermind.rb
@@ -1,7 +1,7 @@
 # Mastermind project
 
 # Randomly set order of 4 numbers between 1-6 (cpu is code maker)
-CODE_NUMBERS = [1, 2, 3, 4, 5, 6]
+CODE_NUMBERS = [1, 2, 3, 4, 5, 6].freeze
 secret_code = CODE_NUMBERS.sample(4)
 
 p secret_code # Delete before going live
@@ -19,4 +19,9 @@ puts "User's guess is a(n) #{user_guess.class}" # Delete before going live
 # Compare guess to code
 # Provide feedback
 # State which numbers and positions are correct
+if secret_code.include?(user_guess)
+  puts "Correct!"
+else
+  puts "Wrong!"
+end
 # User continues to make guesses until code is completely guessed or 10 attempts have been made
