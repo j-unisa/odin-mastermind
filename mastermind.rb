094688f8c0cfa95e4db19c7338416b92142ae7eb
j-unisa <104635026+j-unisa@users.noreply.github.com>
2026-09-02 12:11:48 -0700

    feat: limit player to 10 guessing attempts
    - enforce game rules for maximum allowed turns
    - prevent infinite prompt loop during play
    - establish turn structure for attempt tracking

--- mastermind.rb
+++ mastermind.rb
@@ -10,18 +10,21 @@ p secret_code # Delete before going live
 puts "Welcome to Mastermind!"
 puts "Try to guess the 4 digit code from numbers 1 to 6."
 
-# User (code breaker) makes guess
-user_guess = gets.chomp.to_i
+10.times do
+  # User (code breaker) makes guess
+  puts "Please enter your selection."
+  user_guess = gets.chomp.to_i
 
-puts "User's guess is #{user_guess}" # Delete before going live
-puts "User's guess is a(n) #{user_guess.class}" # Delete before going live
+  puts "User's guess is #{user_guess}" # Delete before going live
+  puts "User's guess is a(n) #{user_guess.class}" # Delete before going live
 
-# Compare guess to code
-# Provide feedback
-# State which numbers and positions are correct
-if secret_code.include?(user_guess)
-  puts "Correct!"
-else
-  puts "Wrong!"
+  # Compare guess to code
+  # Provide feedback
+  # State which numbers and positions are correct
+  if secret_code.include?(user_guess)
+    puts "Correct!"
+  else
+    puts "Wrong!"
+  end
+  # User continues to make guesses until code is completely guessed or 10 attempts have been made
 end
-# User continues to make guesses until code is completely guessed or 10 attempts have been made
